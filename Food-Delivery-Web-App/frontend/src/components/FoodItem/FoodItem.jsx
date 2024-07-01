875192c927dd7b99ace25495a16952551af3b188
Aliff Ashraff <aliffzulkiffli97@gmail.com>
2024-07-01 21:05:53 +0800

    create add to cart function in StoreContextProvider and replace the count item state in the foodItem component

--- Food-Delivery-Web-App/frontend/src/components/FoodItem/FoodItem.jsx
+++ Food-Delivery-Web-App/frontend/src/components/FoodItem/FoodItem.jsx
@@ -1,16 +1,20 @@
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import { assets } from '../../assets/assets';
 import './FoodItem.css';
+import { StoreContext } from '../../context/StoreContext';
 
 // create the structure of the food items from the props
 const FoodItem = ({ id, name, price, description, image }) => {
-  const [itemCount, setItemCount] = useState(0);
+  // const [itemCount, setItemCount] = useState(0);
+  // get values from context to add to cart
+  const { cartItems, addTocart, removeFromCart } = useContext(StoreContext);
 
   return (
     <div className="food-item">
       <div className="food-item-img-container">
         <img className="food-item-image" src={image} alt="" />
-        {itemCount ? (
+        {/* functions without add to cart */}
+        {/* {itemCount ? (
           <div className="food-item-counter">
             <img
               src={assets.remove_icon_red}
@@ -29,6 +33,29 @@ const FoodItem = ({ id, name, price, description, image }) => {
             className="add-food-item"
             src={assets.add_icon_white}
             onClick={() => setItemCount((currentCount) => currentCount + 1)}
+          />
+        )} */}
+
+        {/* pass id as the itemId */}
+        {cartItems[id] ? (
+          <div className="food-item-counter">
+            <img
+              src={assets.remove_icon_red}
+              alt=""
+              onClick={() => removeFromCart(id)}
+            />
+            <p>{cartItems[id]}</p>
+            <img
+              src={assets.add_icon_green}
+              alt=""
+              onClick={() => addTocart(id)}
+            />
+          </div>
+        ) : (
+          <img
+            className="add-food-item"
+            src={assets.add_icon_white}
+            onClick={() => addTocart(id)}
           />
         )}
       </div>
--- Food-Delivery-Web-App/frontend/src/context/StoreContext.jsx
+++ Food-Delivery-Web-App/frontend/src/context/StoreContext.jsx
@@ -1,4 +1,4 @@
-import { createContext } from 'react';
+import { createContext, useEffect, useState } from 'react';
 import { food_list } from '../assets/assets';
 
 // create context with null as default value
@@ -6,9 +6,47 @@ export const StoreContext = createContext(null);
 
 // create context provider component
 const StoreContextProvider = (props) => {
-  // set up food_list array as the context value to be accesed by any component
+  // cart item state
+  const [cartItems, setCartItems] = useState({});
+
+  // add to cart function
+  const addTocart = (itemId) => {
+    // use [] bcs itemId is a variable
+    if (!cartItems[itemId]) {
+      // if user first time adding item in the cart, it will create new entry in the cartItems
+      setCartItems((current) => ({ ...current, [itemId]: 1 }));
+    } else {
+      // add item amount if the item already in the cart
+      setCartItems((current) => ({
+        ...current,
+        [itemId]: current[itemId] + 1,
+      }));
+    }
+  };
+
+  // remove from cart function
+  const removeFromCart = (itemId) => {
+    setCartItems((current) => ({ ...current, [itemId]: current[itemId] - 1 }));
+  };
+
+  // useEffect
+  useEffect(
+    () => {
+      console.log(cartItems);
+    },
+    // run this function everytime cartItem state updated
+    [cartItems]
+  );
+
+  // anything in the context value can be accesed by any component if it is within the provider
   const contextValue = {
+    // set up food_list array
     food_list,
+    // pass the cart state and functions
+    cartItems,
+    setCartItems,
+    addTocart,
+    removeFromCart,
   };
 
   return (
