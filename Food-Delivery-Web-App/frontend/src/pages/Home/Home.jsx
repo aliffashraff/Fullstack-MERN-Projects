745c50013e8017193e0dff91d2e65c4814498a61
Aliff Ashraff <aliffzulkiffli97@gmail.com>
2024-07-01 23:11:01 +0800

    create and style AppDownload component and mount it in home page

--- Food-Delivery-Web-App/frontend/src/pages/Home/Home.jsx
+++ Food-Delivery-Web-App/frontend/src/pages/Home/Home.jsx
@@ -1,3 +1,4 @@
+import AppDownload from '../../components/AppDownload/AppDownload';
 import ExploreMenu from '../../components/ExploreMenu/ExploreMenu';
 import FoodDisplay from '../../components/FoodDisplay/FoodDisplay';
 import Header from '../../components/Header/Header';
@@ -12,6 +13,7 @@ const Home = () => {
       <Header />
       <ExploreMenu category={category} setCategory={setCategory} />
       <FoodDisplay category={category} />
+      <AppDownload />
     </div>
   );
 };
