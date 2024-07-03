40566c8299d6ff898f212bb2c2c7c56c37e013b3
Aliff Ashraff <aliffzulkiffli97@gmail.com>
2024-07-04 02:57:15 +0800

    handle addFood with image req

--- Food-Delivery-Web-App/backend/controllers/foodController.js
+++ Food-Delivery-Web-App/backend/controllers/foodController.js
@@ -6,8 +6,47 @@ import FoodModel from '../models/FoodModel.js';
 
 // add food item
 const addFood = async (req, res) => {
-  const food = await FoodModel.create({ ...req.body });
-  res.status(StatusCodes.CREATED).json({ food });
+  /* // using new FoodModel and await food.save()
+  is good if want to manipulate the food instance 
+  // variable to store uploaded filename
+  let image_filename = `${req.file.filename}`;
+
+  // food instance to access sent details from body
+  const food = new FoodModel({
+    name: req.body.name,
+    description: req.body.description,
+    price: req.body.price,
+    category: req.body.category,
+    image: image_filename,
+  });
+
+  try {
+    // save the instance in database
+    const savedFood = await food.save();
+    res
+      .status(StatusCodes.CREATED)
+      .json({ succes: true, message: 'Food Added', savedFood });
+  } catch (error) {
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ success: false, error: error.message });
+  }*/
+
+  try {
+    const food = await FoodModel.create({
+      ...req.body,
+      image: req.file.filename,
+    });
+    res.status(StatusCodes.CREATED).json({
+      success: true,
+      message: 'Food Added',
+      food,
+    });
+  } catch (error) {
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ success: false, error: error.message });
+  }
 };
 
 export { addFood };
--- Food-Delivery-Web-App/backend/routes/foodRoute.js
+++ Food-Delivery-Web-App/backend/routes/foodRoute.js
@@ -1,12 +1,24 @@
 import express from 'express';
 import { addFood } from '../controllers/foodController.js';
-// import multer to create image storage system
+// import multer to handle file uploads in Express app
 import multer from 'multer';
 
 const foodRouter = express.Router();
 
+// image storage engine - configure how files are stored
+const storage = multer.diskStorage({
+  // stored the file in the uploads folder
+  // no need to provide full path, folder will be created if it is not existed
+  destination: 'uploads',
+  // to create unique filename (timestamp + originalname)
+  filename: (req, file, callback) => {
+    return callback(null, `${Date.now()}${file.originalname}`);
+  },
+});
 
+// middleware to upload the folder
+const upload = multer({ storage: storage });
 
-foodRouter.route('/add').post(addFood);
+foodRouter.route('/add').post(upload.single('image'), addFood);
 
 export default foodRouter;
