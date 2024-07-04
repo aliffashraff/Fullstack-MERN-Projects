3443d4e573bc4443bc3ad6435dfc56da8af86588
Aliff Ashraff <aliffzulkiffli97@gmail.com>
2024-07-05 01:55:56 +0800

    create and style sidebar

--- Food-Delivery-Web-App/admin/src/components/Sidebar/Sidebar.jsx
+++ Food-Delivery-Web-App/admin/src/components/Sidebar/Sidebar.jsx
@@ -1,7 +1,27 @@
+import { assets } from '../../assets/assets';
 import './Sidebar.css';
+import { NavLink } from 'react-router-dom';
 
 const Sidebar = () => {
-  return <div></div>;
+  return (
+    <div className="sidebar">
+      <div className="sidebar-options">
+        {/* navlink will automatic create the active class when clicked */}
+        <NavLink to={'/add'} className="sidebar-option">
+          <img src={assets.add_icon} alt="" />
+          <p>Add Items</p>
+        </NavLink>
+        <NavLink to={'/list'} className="sidebar-option">
+          <img src={assets.order_icon} alt="" />
+          <p>List Items</p>
+        </NavLink>
+        <NavLink to={'/orders'} className="sidebar-option">
+          <img src={assets.order_icon} alt="" />
+          <p>Orders</p>
+        </NavLink>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
