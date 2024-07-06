d750fc7a9e2192ec80b5c64e2a1f1072fe3a6ef8
Aliff Ashraff <aliffzulkiffli97@gmail.com>
2024-07-06 18:54:11 +0800

    add error message

--- Food-Delivery-Web-App/backend/models/FoodModel.js
+++ Food-Delivery-Web-App/backend/models/FoodModel.js
@@ -1,28 +1,31 @@
 import mongoose from 'mongoose';
 
-const FoodSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
+const FoodSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Please provide food name'],
+    },
+    description: {
+      type: String,
+      required: [true, 'Please provide food description'],
+    },
+    price: {
+      type: Number,
+      required: [true, 'Please provide food price'],
+    },
+    // image url
+    image: {
+      type: String,
+      required: [true, 'Please provide food image'],
+    },
+    category: {
+      type: String,
+      required: [true, 'Please provide food category'],
+    },
   },
-  description: {
-    type: String,
-    required: true,
-  },
-  price: {
-    type: Number,
-    required: true,
-  },
-  // image url
-  image: {
-    type: String,
-    required: true,
-  },
-  category: {
-    type: String,
-    required: true,
-  },
-}, {timestamps: true});
+  { timestamps: true }
+);
 
 // checks if the model has already been defined before defining it again
 const FoodModel = mongoose.models.Food || mongoose.model('Food', FoodSchema);
