e16626306a6d2f1ae6c3d2ac183c3afbcd23192d
Aliff Ashraff <aliffzulkiffli97@gmail.com>
2024-07-09 00:32:07 +0800

    add user orders route

--- Food-Delivery-Web-App/backend/routes/orderRoute.js
+++ Food-Delivery-Web-App/backend/routes/orderRoute.js
@@ -1,10 +1,15 @@
 import express from 'express';
-import { placeOrder, verifyOrder } from '../controllers/orderController.js';
+import {
+  placeOrder,
+  userOrders,
+  verifyOrder,
+} from '../controllers/orderController.js';
 import authMiddleware from '../middlewares/authMiddleware.js';
 
 const orderRouter = express.Router();
 
 orderRouter.route('/place').post(authMiddleware, placeOrder);
 orderRouter.route('/verify').post(verifyOrder);
+orderRouter.route('/userorders').post(authMiddleware, userOrders);
 
 export default orderRouter;
