d8dad3e10d28b236334513a7005f52ee4ce96326
Aliff Ashraff <aliffzulkiffli97@gmail.com>
2024-07-09 22:37:38 +0800

    add order empty container

--- Food-Delivery-Web-App/frontend/src/pages/MyOrders/MyOrders.jsx
+++ Food-Delivery-Web-App/frontend/src/pages/MyOrders/MyOrders.jsx
@@ -3,9 +3,10 @@ import './MyOrders.css';
 import { StoreContext } from '../../context/StoreContext';
 import axios from 'axios';
 import { assets } from '../../assets/assets';
+import { Link } from 'react-router-dom';
 
 const MyOrders = () => {
-  const { token, url } = useContext(StoreContext);
+  const { token, url, setMenu } = useContext(StoreContext);
   const [data, setData] = useState([]);
 
   const fetchOrders = async () => {
@@ -19,7 +20,9 @@ const MyOrders = () => {
         }
       );
       setData(response.data.data);
-    } catch (error) {console.log(error);}
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   useEffect(
@@ -34,33 +37,45 @@ const MyOrders = () => {
 
   return (
     <div className="my-orders">
-      <h2>My Orders</h2>
-      <div className="container">
-        {data.map((order) => (
-          <div className="my-orders-order" key={order._id}>
-            <img src={assets.parcel_icon} alt="" />
-            <p>
-              {/* display items name for each order */}
-              {order.items.map((item, index) => {
-                // this if is to remove comma at the end
-                if (index === order.items.length - 1) {
-                  return item.name + ' x ' + item.quantity;
-                } else {
-                  return item.name + ' x ' + item.quantity + ', ';
-                }
-              })}
-            </p>
-            <p>${order.amount}.00</p>
-            <p>Items: {order.items.length}</p>
-            <p>
-              <span>&#x25cf;</span>&nbsp;
-              <b>{order.status}</b>
-            </p>
-            {/* will display the status */}
-            <button onClick={fetchOrders}>Track Order</button>
+      {data.length === 0 ? (
+        <div className="empty-order-container">
+          <h2>You Do Not Have Any Order</h2>
+          <p>Please make a payment to view your orders.</p>
+          <Link to={'/'}>
+            <button onClick={() => setMenu('home')}>View Menu</button>
+          </Link>
+        </div>
+      ) : (
+        <>
+          <h2>My Orders</h2>
+          <div className="container">
+            {data.map((order) => (
+              <div className="my-orders-order" key={order._id}>
+                <img src={assets.parcel_icon} alt="" />
+                <p>
+                  {/* display items name for each order */}
+                  {order.items.map((item, index) => {
+                    // this if is to remove comma at the end
+                    if (index === order.items.length - 1) {
+                      return item.name + ' x ' + item.quantity;
+                    } else {
+                      return item.name + ' x ' + item.quantity + ', ';
+                    }
+                  })}
+                </p>
+                <p>${order.amount}.00</p>
+                <p>Items: {order.items.length}</p>
+                <p>
+                  <span>&#x25cf;</span>&nbsp;
+                  <b>{order.status}</b>
+                </p>
+                {/* will display the status */}
+                <button onClick={fetchOrders}>Track Order</button>
+              </div>
+            ))}
           </div>
-        ))}
-      </div>
+        </>
+      )}
     </div>
   );
 };
