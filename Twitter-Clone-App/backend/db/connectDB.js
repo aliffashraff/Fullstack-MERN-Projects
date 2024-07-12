01278187fa916ae3db3a2d39c7c6957c56ad7080
Aliff Ashraff <aliffzulkiffli97@gmail.com>
2024-07-12 22:59:53 +0800

    connect db

--- Twitter-Clone-App/backend/db/connectDB.js
+++ Twitter-Clone-App/backend/db/connectDB.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const connect = mongoose.connect(process.env.MONGO_URI);
+    const connect = await mongoose.connect(process.env.MONGO_URI);
     console.log('Database is connected: ' + (await connect).connection.host);
   } catch (error) {
     console.log(`Error connection to databas: ${error.message}`);
