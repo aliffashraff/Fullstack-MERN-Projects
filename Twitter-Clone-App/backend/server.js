f54131348041428502c1eb2044da40bee79f76d6
Aliff Ashraff <aliffzulkiffli97@gmail.com>
2024-07-12 22:59:00 +0800

    add urlencoded to send form dat and cookie parse

--- Twitter-Clone-App/backend/server.js
+++ Twitter-Clone-App/backend/server.js
@@ -2,6 +2,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 
 // files
 import connectDB from './db/connectDB.js';
@@ -15,6 +16,8 @@ const port = process.env.PORT || 3000;
 // middlewares
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true })); // to parse form data - can use FormData to send req
+app.use(cookieParser()); // parse cookies in request
 
 // routes
 app.use('/api/auth', authRouter);
