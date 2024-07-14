4d053d3b72d10d770fac4e083e2baae8d4d55c53
Aliff Ashraff <aliffzulkiffli97@gmail.com>
2024-07-14 20:54:17 +0800

    create notification model

---
+++ Twitter-Clone-App/backend/models/NotificationModel.js
@@ -0,0 +1,33 @@
+import mongoose from 'mongoose';
+
+const NotificationSchema = new mongoose.Schema(
+  {
+    from: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    to: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    // type of notification
+    type: {
+      type: String,
+      required: true,
+      enum: ['follow', 'like'],
+    },
+    read: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const NotificationModel =
+  mongoose.models.Notification ||
+  mongoose.model('Notification', NotificationSchema);
+
+export default NotificationModel;
