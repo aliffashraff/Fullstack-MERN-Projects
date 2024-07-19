05e5f596eaefbf205391715d8e1b8447ab82e8ba
Aliff Ashraff <aliffzulkiffli97@gmail.com>
2024-07-20 01:08:25 +0800

    add proxy to the server fro api route

--- Twitter-Clone-App/frontend/vite.config.js
+++ Twitter-Clone-App/frontend/vite.config.js
@@ -4,5 +4,11 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5000 },
+  server: {
+    port: 5000,
+    // whenever there is /api, it will prefix it with the localhost
+    proxy: {
+      '/api': { target: 'http://localhost:3000', changeOrigin: true },
+    },
+  },
 });
