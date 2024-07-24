4cf699203625995e2aebac7683ebe4c46f7543fc
Aliff Ashraff <aliffzulkiffli97@gmail.com>
2024-07-24 16:19:10 +0800

    fetch posts for the homepage

--- Twitter-Clone-App/frontend/src/components/common/Posts.jsx
+++ Twitter-Clone-App/frontend/src/components/common/Posts.jsx
@@ -1,28 +1,70 @@
-import Post from "./Post";
-import PostSkeleton from "../skeletons/PostSkeleton";
-import { POSTS } from "../../utils/db/dummy";
+import Post from './Post';
+import PostSkeleton from '../skeletons/PostSkeleton';
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+import { useEffect } from 'react';
 
-const Posts = () => {
-	const isLoading = false;
+const Posts = ({ feedType }) => {
+  const getPostEndPoint = () => {
+    switch (feedType) {
+      case 'forYou':
+        return '/api/post/all';
+      case 'following':
+        return '/api/post/followings';
+      default:
+        return '/api/post/all';
+    }
+  };
 
-	return (
-		<>
-			{isLoading && (
-				<div className='flex flex-col justify-center'>
-					<PostSkeleton />
-					<PostSkeleton />
-					<PostSkeleton />
-				</div>
-			)}
-			{!isLoading && POSTS?.length === 0 && <p className='text-center my-4'>No posts in this tab. Switch 👻</p>}
-			{!isLoading && POSTS && (
-				<div>
-					{POSTS.map((post) => (
-						<Post key={post._id} post={post} />
-					))}
-				</div>
-			)}
-		</>
-	);
+  const POST_ENDPOINT = getPostEndPoint();
+
+  const {
+    data: posts,
+    isLoading,
+    refetch,
+    isRefetching,
+  } = useQuery({
+    queryKey: ['posts'],
+    queryFn: async () => {
+      try {
+        const response = await axios.get(POST_ENDPOINT);
+
+        if (response.data.success) {
+          console.log(response.data.data);
+          return response.data.data;
+        }
+      } catch (error) {
+        console.error(error);
+        throw new Error(error.response?.data?.error || 'Something went wrong');
+      }
+    },
+  });
+
+  useEffect(() => {
+    // refetch everytime feedType state changes
+    refetch();
+  }, [feedType, refetch]);
+
+  return (
+    <>
+      {(isLoading || isRefetching) && (
+        <div className="flex flex-col justify-center">
+          <PostSkeleton />
+          <PostSkeleton />
+          <PostSkeleton />
+        </div>
+      )}
+      {!isLoading && !isRefetching && posts?.length === 0 && (
+        <p className="text-center my-4">No posts in this tab. Switch 👻</p>
+      )}
+      {!isLoading && !isRefetching && posts && (
+        <div>
+          {posts.map((post) => (
+            <Post key={post._id} post={post} />
+          ))}
+        </div>
+      )}
+    </>
+  );
 };
 export default Posts;
