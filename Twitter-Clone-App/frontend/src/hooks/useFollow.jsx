aa793e115b1e442e3a61c7f67b3b32249962cdbb
Aliff Ashraff <aliffzulkiffli97@gmail.com>
2024-07-29 17:34:26 +0800

    remove api based url

--- Twitter-Clone-App/frontend/src/hooks/useFollow.jsx
+++ Twitter-Clone-App/frontend/src/hooks/useFollow.jsx
@@ -4,12 +4,11 @@ import toast from 'react-hot-toast';
 
 const useFollow = () => {
   const queryClient = useQueryClient();
-  const apiBaseUrl = process.env.REACT_APP_API_BASE_URL;
 
   const { mutate: followUnfollowMutation, isPending } = useMutation({
     mutationFn: async (userId) => {
       try {
-        const url = `${apiBaseUrl}/api/user/follow/${userId}`;
+        const url = `/api/user/follow/${userId}`;
         const response = await axios.post(url);
 
         if (response.data.success) {
